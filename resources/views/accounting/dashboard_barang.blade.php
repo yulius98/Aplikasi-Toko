45bf0514976bfc84cfdbe87d49f772fd170f15a2
yulius k wijaya <70045649+yulius98@users.noreply.github.com>
2025-05-31 16:19:37 +0700

    commit4

--- resources/views/accounting/dashboard_barang.blade.php
+++ resources/views/accounting/dashboard_barang.blade.php
@@ -37,15 +37,9 @@
                         </div>
                     </div>
                 </main>
+                <x-footer />
 
-
-                <footer class="py-4 bg-light mt-auto">
-                    <div class="container-fluid px-4">
-                        <div class="d-flex align-items-center justify-content-between small">
-                            <div class="text-muted">Copyright &copy; Your Website 2023</div>
-                        </div>
-                    </div>
-                </footer>
+                
             </div>
         </div>
         {{-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script> --}}
--- resources/views/accounting/dashboard_supplier.blade.php
+++ resources/views/accounting/dashboard_supplier.blade.php
@@ -30,13 +30,7 @@
 
 
                 </main>
-                <footer class="py-4 bg-light mt-auto">
-                    <div class="container-fluid px-4">
-                        <div class="d-flex align-items-center justify-content-between small">
-                            <div class="text-muted">Copyright &copy; Your Website 2023</div>
-                        </div>
-                    </div>
-                </footer>
+                <x-footer />
             </div>
         </div>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
